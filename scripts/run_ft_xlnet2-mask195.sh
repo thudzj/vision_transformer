959f5e6c662b80b1fab3ede41dde850c1a3fdd5d
thudzj <dengzhijiethu@gmail.com>
2022-01-08 15:33:56 +0800

    Update scripts

---
+++ scripts/run_ft_xlnet2-mask195.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=ft_xlnet2-mask195    # create a short name for your job
+#SBATCH --output=log_ft_xlnet2-mask195.txt
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks-per-node=1      # total number of tasks across all nodes
+#SBATCH --cpus-per-task=24        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem=64G                 # total memory per node (4 GB per cpu-core is default)
+#SBATCH --gres=gpu:4             # number of gpus per node
+#SBATCH --time=202:00:00          # total run time limit (HH:MM:SS)
+
+module load nvidia/cuda/11.4
+
+source /apps/local/conda_init.sh
+conda activate hao_vit
+rm -rf /l/users/hongyiwa/hao/vision_transformer/ft_xlnet2-mask195
+
+XLA_FLAGS=--xla_gpu_cuda_data_dir=/usr/lib/cuda python3 -m vit_jax.main --workdir=/l/users/hongyiwa/hao/vision_transformer/ft_xlnet2-mask195 --config=/l/users/hongyiwa/hao/vision_transformer/vit_jax/configs/ft.py:b16 --config.dataset=/l/users/hongyiwa/datasets/ILSVRC2012 --config.pretrained_path=./xlnet2-mask195 --config.batch=1024 --config.batch_eval=1024
